130d5cb9ef50bc653439ba5cfba2aa916867fc47
Nikola <n_kasabov@mail.bg>
2023-01-10 16:10:05 +0200

    add Header component

--- url-shortening-api-react/src/components/Header/Header.js
+++ url-shortening-api-react/src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import './Header.scss';
 import logo from '../../images/logo.svg';
+import Nav from '../Nav/Nav';
 function Header() {
 
   return (
     <header className='header'>
       <img src={logo} alt="logo" />
+      <Nav />
     </header>
   );
 }
--- url-shortening-api-react/src/components/Nav/Nav.js
+++ url-shortening-api-react/src/components/Nav/Nav.js
@@ -1,13 +1,34 @@
 import './Nav.scss';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from 'react';
 
 function Nav() {
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+
+  function openNav() {
+    setIsMobileOpen(true);
+  }
+
+  function closeNav() {
+    setIsMobileOpen(false);
+  }
+
   return (
     <nav className="nav">
-      <ul>
-        <li><a href='#'>Features</a></li>
-        <li><a href='#'>Pricing</a></li>
-        <li><a href='#'>Resources</a></li>
+      <div className={`nav-overlay ${isMobileOpen ? 'open' : ''}`}
+        onClick={closeNav}
+      ></div>
+      <ul className={`nav-links ${isMobileOpen ? 'open' : ''}`}>
+        <li><a href='#' onClick={closeNav}>Features</a></li>
+        <li><a href='#' onClick={closeNav}>Pricing</a></li>
+        <li><a href='#' onClick={closeNav}>Resources</a></li>
+        <div className="nav-line"></div>
+        <li className='nav-login'><a href='#' onClick={closeNav}>Login</a></li>
+        <li><a href='#' className='btn nav-sign-up' onClick={closeNav}>Sign Up</a></li>
       </ul>
+      <button className='nav-open-btn' onClick={openNav}>
+        <RxHamburgerMenu className='nav-open-btn-icon' />
+      </button>
     </nav>
   );
 }
